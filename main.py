3b29167a22ba0b823232f81f45a7fbd391a46f15
Nicolas <nicolas.beames04@gmail.com>
2023-10-27 19:21:39 -0300

    Colisao funcionando

--- main.py
+++ main.py
@@ -2,30 +2,54 @@ import os
 import pygame
 
 from sprites import Player
+from colisao import Colisao
+from rects import Rect
 
+#coordenadas dos retangulos de colisão
+coords = [
+      [65,60,215,80], 
+      [575,70,140,65],
+      [1010,75,200,60],
+
+      [100,335,170,50],
+      [560,325,170,60],
+      [1050,320,175,70],
+
+      [75,585,180,50],
+      [600,550,90,110],
+      [1000,585,210,65]
+   ]
+
+#iterador para colisão
+i = range(8)
 
 def main():
    pygame.init()
+   pygame.font.init()
 
    #Janela e fundo
-   window_size = (1200, 700)
+   window_size = (1280, 720)
    window = pygame.display.set_mode(window_size)
-   
    pygame.display.set_caption("Planeta Kids")
    background = pygame.image.load(os.path.join("Arquivos", "FundoGame.jpg"))
    background = pygame.transform.scale(background, (1280, 720))
    window.blit(background, (0,0))
 
+   #Travar fps em 60 
    clock = pygame.time.Clock()
    clock.tick(60)
 
    #Criar Personagem
-   player = Player()   # spawn player
-   player.rect.x = 0   # go to x
-   player.rect.y = 0   # go to y
+   player = Player()
+   player.rect.x = 605  # vai para x=0
+   player.rect.y = 415  # vai para y=0
    player_list = pygame.sprite.Group()
    player_list.add(player)
 
+   #Criar retangulos e colisão
+   for i in range(len(coords)):
+      x, y, width, height = coords[i]
+      globals()["colis" + str(i)] = Rect(x, y, width, height)
 
    #Loop do jogo
    running = True
@@ -36,9 +60,42 @@ def main():
             running = False
 
 
-      player_list.draw(window) #Desenhar personagem
+      #Obtenha as teclas pressionadas
+      keys = pygame.key.get_pressed()
+
+      #Atualize o jogador com base nas teclas pressionadas
       
 
+      window.blit(background, (0, 0))  #Desenhe o fundo novamente para limpar a tela
+      player_list.draw(window)  #Desenhe o jogador
+      pygame.display.flip()
+      clock.tick(60)
+      
+
+      #Colisão com objetos
+      if player.rect.colliderect(colis0):
+         player.update(keys, window_size[0], window_size[1], Colisao.zerarVel("col0", player.rect, colis0.rect, 10))
+      elif player.rect.colliderect(colis1):
+         player.update(keys, window_size[0], window_size[1], Colisao.zerarVel("col1", player.rect, colis1.rect, 10))
+      elif player.rect.colliderect(colis2):
+         player.update(keys, window_size[0], window_size[1], Colisao.zerarVel("col2", player.rect, colis2.rect, 10))
+      elif player.rect.colliderect(colis3):
+         player.update(keys, window_size[0], window_size[1], Colisao.zerarVel("col3", player.rect, colis3.rect, 10))
+      elif player.rect.colliderect(colis4):
+         player.update(keys, window_size[0], window_size[1], Colisao.zerarVel("col4", player.rect, colis4.rect, 10))
+      elif player.rect.colliderect(colis5):
+         player.update(keys, window_size[0], window_size[1], Colisao.zerarVel("col5", player.rect, colis5.rect, 10))
+      elif player.rect.colliderect(colis6):
+         player.update(keys, window_size[0], window_size[1], Colisao.zerarVel("col6", player.rect, colis6.rect, 10))
+      elif player.rect.colliderect(colis7):
+         player.update(keys, window_size[0], window_size[1], Colisao.zerarVel("col7", player.rect, colis7.rect, 10))
+      elif player.rect.colliderect(colis8):
+         player.update(keys, window_size[0], window_size[1], Colisao.zerarVel("col8", player.rect, colis8.rect, 10))
+      else:
+         player.update(keys, window_size[0], window_size[1], 0)
+      #x, y = pygame.mouse.get_pos()
+      #pos = myfont.render(f'x = {x} y = {y}', False, (0, 0, 0))
+      #window.blit(pos, (990, 30))
 
       pygame.display.flip()
    
--- sprites.py
+++ sprites.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -8,7 +9,37 @@ class Player(pygame.sprite.Sprite):
 
         img = pygame.image.load(os.path.join('Arquivos', 'pixil-frame-0.png')).convert_alpha()
         self.size = img.get_size()
-        self.smaller = pygame.transform.scale(img, (int(self.size[0]*0.15), int(self.size[1]*0.15)))
+        self.smaller = pygame.transform.scale(img, (int(self.size[0] * 0.15), int(self.size[1] * 0.15)))
         self.images.append(self.smaller)
         self.image = self.images[0]
         self.rect = self.image.get_rect()
+
+    # def update(self, keys):
+    #     #Tecla pressionada e velocidade
+    #     # if keys[pygame.K_LEFT]:
+    #     #     self.rect.x -= 3
+    #     # if keys[pygame.K_RIGHT]:
+    #     #     self.rect.x += 3
+    #     # if keys[pygame.K_UP]:
+    #     #     self.rect.y -= 3
+    #     # if keys[pygame.K_DOWN]:
+    #     #     self.rect.y += 3
+    #     
+
+    def update(self, keys, screen_width, screen_height, col):
+        if keys[pygame.K_LEFT] and not col == "left":
+            self.rect.x -= 5
+            if self.rect.right < 0:
+                self.rect.left = screen_width
+        if keys[pygame.K_RIGHT] and not col == "right":
+            self.rect.x += 5
+            if self.rect.left > screen_width:
+                self.rect.right = 0
+        if keys[pygame.K_UP] and not col == "up":
+            self.rect.y -= 5
+            if self.rect.bottom < 0:
+                self.rect.top = screen_height
+        if keys[pygame.K_DOWN] and not col == "down":
+            self.rect.y += 5
+            if self.rect.top > screen_height:
+                self.rect.bottom = 0
